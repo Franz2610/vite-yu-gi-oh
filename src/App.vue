ad695ea1c511953cdd980ffaf8e925d3e338bfe5
Franz2610 <francescofroechlich1@gmail.com>
2023-04-17 15:38:10 +0200

    scaffholnding

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { store } from './store';
+import axios from 'axios';
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
